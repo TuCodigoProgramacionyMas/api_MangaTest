d688d8b09a0ab06d04e5ef7833248745128a13ef
Rolando Perez sanchez <rolando.perez@uteco.edu.do>
2023-06-20 14:10:29 -0400

    ...

--- index.js
+++ index.js
@@ -1,34 +1,90 @@
-const express = require('express');
-const chromium = require('chrome-aws-lambda');
+// const express = require('express');
+// const chromium = require('chrome-aws-lambda');
+
+// const app = express();
+// const port = 3000;
+
+// app.get('/scrape', async (req, res) => {
+//   let url = req.query.url;
+
+//   url='https://leermanga.net/';
+//   if (!url) {
+//     return res.json({ error: 'Missing URL parameter' });
+//   }
+
+//   const browser = await chromium.puppeteer.launch({
+//     args: chromium.args,
+//     defaultViewport: chromium.defaultViewport,
+//     executablePath: await chromium.executablePath,
+//     headless: false, // Modificar esta línea
+//   });
+
+//   const webPage = await browser.newPage();
+
+//   // Bloquear las solicitudes CSS
+//   await webPage.setRequestInterception(true);
+//   webPage.on('request', (request) => {
+//     if (request.resourceType() === 'stylesheet') {
+//       request.abort();
+//     } else {
+//       request.continue();
+//     }
+//   });
+
+//   await webPage.goto(url);
+
+//   const pageContent = await webPage.content();
+
+//   await browser.close();
+
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(JSON.stringify({ html: pageContent }));
+// });
+
+// app.listen(port, () => {
+//   console.log(`App running on http://localhost:${port}`);
+// });
+
+const chromium = require("chrome-aws-lambda");
+const express = require("express");
+const puppeteer = require("puppeteer");
 
 const app = express();
 const port = 3000;
 
-app.get('/scrape', async (req, res) => {
+app.get("/scrape", async (req, res) => {
   const url = req.query.url;
 
   if (!url) {
-    return res.json({ error: 'Missing URL parameter' });
+    return res.json({ error: "Missing URL parameter" });
   }
-
-  const browser = await chromium.puppeteer.launch({
+ 
+  // const executablePath = await chromium.executablePath;
+  // if (!executablePath) {
+  //   return puppeteer.launch({
+  //     args: chromium.args,
+  //     headless: chromium.headless,
+  //     ignoreHTTPSErrors: true,
+  //   });
+  // }
+  
+  const browser = await puppeteer.launch({
     args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: false, // Modificar esta línea
+    headless: true,  // <-- Configura esto en true para ocultar la ventana del navegador
+    ignoreHTTPSErrors: true,
   });
 
   const webPage = await browser.newPage();
 
   // Bloquear las solicitudes CSS
-  await webPage.setRequestInterception(true);
-  webPage.on('request', (request) => {
-    if (request.resourceType() === 'stylesheet') {
-      request.abort();
-    } else {
-      request.continue();
-    }
-  });
+  // await webPage.setRequestInterception(true);
+  // webPage.on("request", (request) => {
+  //   if (request.resourceType() === "stylesheet") {
+  //     request.abort();
+  //   } else {
+  //     request.continue();
+  //   }
+  // });
 
   await webPage.goto(url);
 
@@ -36,7 +92,7 @@ app.get('/scrape', async (req, res) => {
 
   await browser.close();
 
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({ html: pageContent }));
 });
 
