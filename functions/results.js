4397ab5872f6f328eec49cf429f9c3da3d01b8bb
Rolando Perez sanchez <rolando.perez@uteco.edu.do>
2023-06-23 15:03:39 -0400

    ....

--- functions/results.js
+++ functions/results.js
@@ -4,25 +4,16 @@ const puppeteer = require('puppeteer');
 exports.handler = async function(event, context) {
   const browser = await puppeteer.launch({
     args: chromium.args,
-    executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
-    headless: true,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
   });
 
   const page = await browser.newPage();
 
   await page.goto('https://spacejelly.dev/');
 
-  await page.focus('#search-query')
-  await page.keyboard.type('api');
-
-  const results = await page.$$eval('#search-query + div a', (links) => {
-    return links.map(link => {
-      return {
-        text: link.innerText,
-        href: link.href
-      }
-    });
-  });
+  const title = await page.title();
+  const description = await page.$eval('meta[name="description"]', element => element.content);
 
   await browser.close();
 
@@ -30,7 +21,10 @@ exports.handler = async function(event, context) {
     statusCode: 200,
     body: JSON.stringify({
       status: 'Ok',
-      results
+      page: {
+        title,
+        description
+      }
     })
   };
-}
+};
